0e97d45c86512316c96fb829783d7641a66b8412
Huneshwar97 <huneshwaryadav123@gmail.com>
2024-02-09 18:11:33 +0530

    create db and updated readme

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require('body-parser')
 
-const {PORT} = require('./config/servrConfig')
+const {PORT} = require('./config/serverConfig')
 
 const setupAndAtartServer = async () =>{
 
