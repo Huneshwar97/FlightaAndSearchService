b1b89af0dc3a46c9adddfbff240d807fe9ac9e44
Huneshwar97 <huneshwaryadav123@gmail.com>
2024-02-18 17:29:50 +0530

    added airport create controller

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const CityController =require('../../controllers/city-controller');
 const FlightContoller =require('../../controllers/flight-controller');
+const AirportController=require('../../controllers/airport-controller');
 const router = express.Router();
 
 router.post('/city',CityController.create);
@@ -11,4 +12,6 @@ router.patch('/city/:id',CityController.update);
 
 router.post('/flight',FlightContoller.create);
 router.get('/flight',FlightContoller.getAll);
+
+router.post('/airports',AirportController.create);
 module.exports = router;
